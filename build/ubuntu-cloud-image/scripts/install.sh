f4d9a93640fd8952e0e30b2914dcdec4372deaa8
DL6544 <DL6544@engie.com>
2024-09-26 20:49:11 +0200

    Mikeshop image created, playing around with customizations.

--- build/ubuntu-cloud-image/scripts/install.sh
+++ build/ubuntu-cloud-image/scripts/install.sh
@@ -8,6 +8,7 @@ exec 2>&1
 # Enable extended globbing
 shopt -s extglob
 
+
 # --  Environment Variables  --
 
 # set var to log path
@@ -21,6 +22,10 @@ NEW_HOSTNAME="${ROLE}-${ARCH}-${ENV}-${COUNTER}"
 MOTD_DIR="/etc/update-motd.d"
 BACKUP_DIR="/etc/update-motd.d/backup"
 CUSTOM_SCRIPT="${MOTD_DIR}/00-mikeshop"
+#vars for user config
+USER_NAME="sysadmin"  # Replace with the username you want to create
+SSH_PUBLIC_KEY="AAAAB3NzaC1yc2EAAAADAQABAAACAQDlP/4lJptihdac/RmC+ZWH/XAh7vCehd6yC6/Zist2D+VlWl6v3p0zRE54Gn3wk5DOymhh4sUTT3zuMIokZMPvwinCo+zR6gD7wU0ATYeRZgX8nn6TLEaMXXYjyCIYZPjUXTs4vYJyHVVaZn6cfATk1DG7VtQBgbveyawp9PpLb0G989gt7wxlAaQx1qVpBywwUB7867DNCmYWJH/1gbsz5jNlKgbbn/og/2RMGL3rrgxJ3BQ9O9GjAYb99AqLdeOSx7TKW1vOL+8JDkPpps2RgTINTexwVZWivyEM/3WeFGyOaZVqSpXSTvhEm8E4AmvuvZNJRxQ0JNZd1io/aMpb5Zo1xV/aunX7voLQZ0V1pWNlBvXBjIVUrT7R7Mwmeub5CT1jr+70qhlKP8z4GA/yZXJNlS88mnTqhwngbXU5jdJdFOlFkCbsR/ofOs2n6q5G+H9HtWs8I0S4iJhXSgqDPknaWUZrGH/HT0ux4KJAjdji7TwA5iJvPeV6SJs4F4hz1enW6UQDRhkIRZi1s4CKWGEAPQwULWq+Lxde6TmPnlLoEJzydNohM8AP7e+EQcGYdjEr7rBmV+ihwpvl1QwF6ToPksShX88kWBAL/AaD1hRE7McAeworojhKOoRQ5/O4P9zuY5BJFxmbNXSwHyMBTmJEGmIRQjI4CKxf1XomjQ=="  # Replace with your actual public key
+
 
 # -- Main Script Section --
 
@@ -39,8 +44,6 @@ if [ -n "$NEW_HOSTNAME" ]; then
 else
   echo "The variable for hostname generation was empty. Cannot set hostname" >> $LOG
 fi
-
-
 # Create a backup folder
 sudo mkdir -p "$BACKUP_DIR"
 
@@ -51,18 +54,42 @@ if ls -A "$MOTD_DIR" | grep -q -v 'backup'; then
 else
     echo "No existing MOTD scripts to back up."
 fi
-
 # Create a custom neofetch MOTD script
 echo "Setting up neofetch as the new MOTD..."
 cat <<EOF | sudo tee $CUSTOM_SCRIPT
 #!/bin/bash
 neofetch
 EOF
-
 # Make the new MOTD script executable
 sudo chmod +x $CUSTOM_SCRIPT
 
 echo "Neofetch has been set as the MOTD. Backup of old scripts is in $BACKUP_DIR." >> $LOG
+
+
+# -- Security Hardening --
+
+# Disable password auth and root login
+sudo sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+# Don't restart service, should only be applied after provisioning process
+# Create the user and set the public key for SSH authentication
+echo "==> Creating user and setting up SSH public key..."
+sudo useradd -m -s /bin/bash "$USER_NAME"
+sudo mkdir -p /home/"$USER_NAME"/.ssh
+echo "$SSH_PUBLIC_KEY" | sudo tee /home/"$USER_NAME"/.ssh/authorized_keys
+sudo chown -R "$USER_NAME":"$USER_NAME" /home/"$USER_NAME"/.ssh
+sudo chmod 700 /home/"$USER_NAME"/.ssh
+sudo chmod 600 /home/"$USER_NAME"/.ssh/authorized_keys
+# Add the user to the sudo group
+echo "==> Adding $USER_NAME to the sudo group..."
+sudo usermod -aG sudo "$USER_NAME"
+
+echo "User $USER_NAME created, SSH key added, and user added to sudo group." >> $LOG
+
+
+
+
 # ---
 
 # Rework from here, add below section
