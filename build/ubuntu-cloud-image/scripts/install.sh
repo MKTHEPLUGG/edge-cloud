7f1f730beede7386df456217cee3e7a0ad2759ac
DL6544 <DL6544@engie.com>
2024-09-26 23:33:28 +0200

    Mikeshop image created, playing around with customizations.

--- build/ubuntu-cloud-image/scripts/install.sh
+++ build/ubuntu-cloud-image/scripts/install.sh
@@ -37,11 +37,6 @@ echo "==> Waiting for Cloud-Init to finish..."
 cloud-init status --wait
 echo "Cloud-Init finished."
 
-zsh --version
-sudo apt update -y && apt upgrade -y
-sudo apt install git zsh
-zsh --version
-
 
 # Configure hostname via variables supplied in the user-data file during the cloud init process.
 if [ -n "$NEW_HOSTNAME" ]; then
@@ -79,9 +74,10 @@ echo "Neofetch has been set as the MOTD. Backup of old scripts is in $BACKUP_DIR
 
 # z-shell setup
 # install zi ( package manager for zsh )
+git clone --depth=1 https://github.com/z-shell/zi.git $ZI_HOME/bin
 sh -c "$(curl -fsSL https://git.io/get-zi)"
 source /home/ubuntu/.zshrc
-chown -R ubuntu:ubuntu /home/ubuntu/.zi
+#chown -R ubuntu:ubuntu /home/ubuntu/.zi
 
 cat <<EOF | sudo tee -a /home/ubuntu/.zshrc
 # Initialize zi
